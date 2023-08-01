73303c900afbcbb4027a3110283c2bc25528b244
lumurga <mlucianamurga@gmail.com>
2023-07-31 22:21:26 -0300

    Clase 5 - Flyweight

---
+++ clase5/src/com/backend/digitalhouse/clase/Main.java
@@ -0,0 +1,23 @@
+package com.backend.digitalhouse.clase;
+
+public class Main {
+    public static void main(String[] args) {
+        Computadora windows1 = ComputadoraFactory.obtenerComputadora(2, 128);
+        Computadora mac1 = ComputadoraFactory.obtenerComputadora(16, 500);
+
+        Computadora windows2 = ComputadoraFactory.obtenerComputadora(2, 256);
+        Computadora mac2 = ComputadoraFactory.obtenerComputadora(8, 500);
+
+        System.out.println("---------------------------------");
+        System.out.println(windows1);
+        System.out.println(windows2);
+        System.out.println(ComputadoraFactory.getComputadorasLigeras().size());
+
+
+    }
+
+
+
+
+
+}
