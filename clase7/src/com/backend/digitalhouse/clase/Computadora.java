acdd8a25794a60f2c80f7b88a8b12c84039d76f9
lumurga <mlucianamurga@gmail.com>
2023-08-02 22:17:04 -0300

    Clase 7 - Logging

---
+++ clase7/src/com/backend/digitalhouse/clase/Computadora.java
@@ -0,0 +1,57 @@
+package com.backend.digitalhouse.clase;
+
+public class Computadora {
+    private String id;
+    private int ram;
+    private int disco;
+    private static int contador;
+
+    public Computadora(int ram, int disco){
+        this.ram = ram;
+        this.disco = disco;
+        this.id = "AR"+ram+"-"+disco;
+        contador++;
+        System.out.println("Existencias: " + contador);
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getDisco() {
+        return disco;
+    }
+
+    public void setDisco(int disco) {
+        this.disco = disco;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public static void setContador(int contador) {
+        Computadora.contador = contador;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "ID: " + id + "\n" +
+                "Ram: " + ram + "\n" +
+                "Disco: " + disco;
+    }
+}
---
+++ clase7/src/com/backend/digitalhouse/clase/ComputadoraFactory.java
@@ -0,0 +1,37 @@
+package com.backend.digitalhouse.clase;
+
+import org.apache.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ComputadoraFactory {
+         //tipo de dato de la key, tipo de dato del objeto asociado a esa key
+    private static Map<String, Computadora> computadorasLigeras = new HashMap<>();
+    private static final Logger LOGGER = Logger.getLogger(ComputadoraFactory.class);
+
+    public static Computadora obtenerComputadora(int ram, int disco) {
+        //definir la key, que no es = al id del objeto computadora
+        String key = "key:" + ram + ":" + disco;
+        //System.out.println(key);
+        //verificar si ya existe esa key en la coleccion
+        if(!computadorasLigeras.containsKey(key)){
+            //crear la compu y la agregar con su key a la coleccion
+            Computadora computadora = new Computadora(ram, disco);
+            computadorasLigeras.put(key, computadora);
+            //computadorasLigeras.put(key, new Computadora(ram, disco));
+            LOGGER.info("Computadora creada: \n" + computadora);
+            //System.out.println("Computadora creada");
+        } else {
+            //System.out.println("Computadora encontrada");
+            LOGGER.info("Computadora encontrada: \n" + computadorasLigeras.get(key));
+        }
+
+        return computadorasLigeras.get(key);
+
+    }
+
+    public static Map<String, Computadora> getComputadorasLigeras() {
+        return computadorasLigeras;
+    }
+}
